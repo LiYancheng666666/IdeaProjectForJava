1fd52e4288208e080e52b992c2860a7efbd6c7fd
leoli <1564509789@qq.com>
2019-01-03 13:01:06 +0800

    update helloworld

---
+++ HelloWorld/src/com/infor/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C)，2019-2019,Infor
+ * Author: lli3
+ * Description First Project
+ */
+
+package com.infor.HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
